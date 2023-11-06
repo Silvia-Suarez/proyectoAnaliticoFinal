5c894102fa5c2057144b262548560e3ec04e9e27
Mauricio Mery <mauricio.mer0527@gmail.com>
2023-11-05 19:57:19 -0500

    Including JSON to CSV

--- etl_pipeline_transport_raw_data.py
+++ etl_pipeline_transport_raw_data.py
@@ -1,6 +1,15 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import pandas as pd
+import json
+
+# Function to convert JSON lines to CSV string
+def json_to_csv(line):
+    # Parse each JSON line
+    data = json.loads(line)
+    # Convert JSON data to CSV string (example, modify as per your JSON structure)
+    csv_data = ','.join([str(value) for value in data.values()])
+    return csv_data
 
 # Function to create Pandas DataFrame from PCollection
 def create_dataframe(data):
@@ -34,16 +43,20 @@ with beam.Pipeline(options=options) as p:
     # Read each CSV file and create a Pandas DataFrame
     dataframes = {}
     for file in input_files:
-        data = p | f"Read {file}" >> beam.io.ReadFromText(file)
-    # Transformation: Split lines and combine fields
-        transformed_data = (
-        data
-        | "SplitLines" >> beam.Map(lambda line: line.split(','))  # Split CSV by comma
-        | "CombineFields" >> beam.Map(lambda fields: ','.join(fields))  # Combine fields back into CSV
-        )
-
-
-        dataframes[file] = transformed_data | f"Create DataFrame {file}" >> beam.Map(create_dataframe)
+        if file.endswith('.json'):
+            # Read the JSON file and transform it to CSV
+            json_data = p | f"Read {file}" >> beam.io.ReadFromText(file)
+            transformed_json = json_data | f"JSONToCSV" >> beam.Map(json_to_csv)
+            dataframes[file] = transformed_json | f"Create DataFrame {file}" >> beam.Map(create_dataframe)
+        else:
+            # Read each CSV file and create a Pandas DataFrame
+            data = p | f"Read {file}" >> beam.io.ReadFromText(file)
+            transformed_data = (
+                data
+                | f"SplitLines" >> beam.Map(lambda line: line.split(','))
+                | f"CombineFields" >> beam.Map(lambda fields: ','.join(fields))
+            )
+            dataframes[file] = transformed_data | f"Create DataFrame {file}" >> beam.Map(create_dataframe)
 
     # You can access each DataFrame like this:
     cine_colombia_peliculas_df = dataframes["gs://transporte_grupo_4/CineColombia_peliculas.csv"]
@@ -54,5 +67,9 @@ with beam.Pipeline(options=options) as p:
     # This part can be modified to write the dataframes to GCS or perform other operations
     # For example:
     for file, dataframe in dataframes.items():
-        dataframe.to_csv(f"gs://cruda_grupo_4/first-trasform-data/{file.split('/')[-1]}_output.csv", index=False)
+        if file.endswith('.json'):
+            # For JSON file, write the DataFrame to CSV in GCS
+            dataframe.to_csv(f"gs://your-bucket/output-data/{file.split('/')[-1].split('.')[0]}_output.csv", index=False)
+        else:
+            dataframe.to_csv(f"gs://cruda_grupo_4/first-trasform-data/{file.split('/')[-1]}_output.csv", index=False)
     #Mery pipeline
