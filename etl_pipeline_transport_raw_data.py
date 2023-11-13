04b934d1dfc4ed78e916236872b043755f1ed16a
Silvia-Suarez <silviznatalia@gmail.com>
2023-11-13 16:35:24 -0500

    Creating requirements for py 3.11, adding json files in etl pipeline and uncommenting some important lines

--- etl_pipeline_transport_raw_data.py
+++ etl_pipeline_transport_raw_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json
 
 # Function to convert JSON lines to CSV string
+
+
 def json_to_csv(line):
     # Parse each JSON line
     data = json.loads(line)
@@ -12,14 +14,17 @@ def json_to_csv(line):
     return csv_data
 
 # Function to create Pandas DataFrame from PCollection
+
+
 def create_dataframe(data):
     # Convert PCollection to a list
     data_list = data | "Collect as List" >> beam.combiners.ToList()
-    
+
     # Create a Pandas DataFrame from the list
     df = pd.DataFrame(data_list)
-    
+
     return df
+
 
 # Set up pipeline options
 options = PipelineOptions()
@@ -37,7 +42,10 @@ with beam.Pipeline(options=options) as p:
         "gs://transporte_grupo_4/CineMark_visualizaciones.csv",
         "gs://transporte_grupo_4/Procinal_peliculas.csv",
         "gs://transporte_grupo_4/Procinal_usuarios.csv",
-        "gs://transporte_grupo_4/Procinal_visualizaciones.csv"
+        "gs://transporte_grupo_4/Procinal_visualizaciones.csv",
+        "gs://transporte_grupo_4/UniCine_peliculas.csv",
+        "gs://transporte_grupo_4/UniCine_usuarios.csv",
+        "gs://transporte_grupo_4/UniCine_visualizaciones.csv"
     ]
 
     # Read each CSV file and create a Pandas DataFrame
@@ -46,8 +54,10 @@ with beam.Pipeline(options=options) as p:
         if file.endswith('.json'):
             # Read the JSON file and transform it to CSV
             json_data = p | f"Read {file}" >> beam.io.ReadFromText(file)
-            transformed_json = json_data | f"JSONToCSV" >> beam.Map(json_to_csv)
-            dataframes[file] = transformed_json | f"Create DataFrame {file}" >> beam.Map(create_dataframe)
+            transformed_json = json_data | f"JSONToCSV" >> beam.Map(
+                json_to_csv)
+            dataframes[file] = transformed_json | f"Create DataFrame {file}" >> beam.Map(
+                create_dataframe)
         else:
             # Read each CSV file and create a Pandas DataFrame
             data = p | f"Read {file}" >> beam.io.ReadFromText(file)
@@ -56,7 +66,8 @@ with beam.Pipeline(options=options) as p:
                 | f"SplitLines" >> beam.Map(lambda line: line.split(','))
                 | f"CombineFields" >> beam.Map(lambda fields: ','.join(fields))
             )
-            dataframes[file] = transformed_data | f"Create DataFrame {file}" >> beam.Map(create_dataframe)
+            dataframes[file] = transformed_data | f"Create DataFrame {file}" >> beam.Map(
+                create_dataframe)
 
     # You can access each DataFrame like this:
     cine_colombia_peliculas_df = dataframes["gs://transporte_grupo_4/CineColombia_peliculas.csv"]
@@ -69,7 +80,17 @@ with beam.Pipeline(options=options) as p:
     for file, dataframe in dataframes.items():
         if file.endswith('.json'):
             # For JSON file, write the DataFrame to CSV in GCS
-            dataframe.to_csv(f"gs://your-bucket/output-data/{file.split('/')[-1].split('.')[0]}_output.csv", index=False)
+            dataframe.to_csv(
+                f"gs://your-bucket/output-data/{file.split('/')[-1].split('.')[0]}_output.csv", index=False)
         else:
-            dataframe.to_csv(f"gs://cruda_grupo_4/first-trasform-data/{file.split('/')[-1]}_output.csv", index=False)
-    #Mery pipeline
+            dataframe.to_csv(
+                f"gs://cruda_grupo_4/first-trasform-data/{file.split('/')[-1]}_output.csv", index=False)
+    # Mery pipeline
+
+# python etl_pipeline_transport_raw_data.py ^
+#   --project datalake-dev-399421 ^
+#   --job_name etl-pipeline-transporte-1 ^
+#   --temp_location gs://transporte_grupo_4/temp ^
+#   --staging_location gs://transporte_grupo_4/staging ^
+#   --runner DataflowRunner ^
+#   --region us-east1
--- upload_data.py
+++ upload_data.py
@@ -32,22 +32,22 @@ for db in DB:
         filename = db+'_' + 'peliculas' + '.json'
         df_peliculas.to_json(index=False, date_format='%Y-%M-%d',
                              path_or_buf=f'data/{filename}')
-    # else:
-    #     df_visualizaciones = pd.read_sql(
-    #         '''SELECT * FROM visualizaciones;''', conn)
-    #     filename = db+'_' + 'visualizaciones' + '.csv'
-    #     df_visualizaciones.to_csv(
-    #         index=False, date_format='%Y-%M-%d', path_or_buf=f'data/{filename}')
-    #     print(f'df_visualizaciones de {db} creado exitosamente')
-    #     df_usuarios = pd.read_sql('''SELECT * FROM usuarios;''', conn)
-    #     # filename = db+'_' + str(datetime.now().date()) + ' _usuarios' + '.csv'
-    #     filename = db+'_' + 'usuarios' + '.csv'
-    #     df_usuarios.to_csv(index=False, date_format='%Y-%M-%d',
-    #                        path_or_buf=f'data/{filename}')
-    #     print(f'df_usuarios de {db} creado exitosamente')
-    #     df_peliculas = pd.read_sql('''SELECT * FROM peliculas;''', conn)
-    #     # filename = db+'_' + str(datetime.now().date()) + '_peliculas' + '.csv'
-    #     filename = db+'_' + 'peliculas' + '.csv'
-    #     df_peliculas.to_csv(index=False, date_format='%Y-%M-%d',
-    #                         path_or_buf=f'data/{filename}')
+    else:
+        df_visualizaciones = pd.read_sql(
+            '''SELECT * FROM visualizaciones;''', conn)
+        filename = db+'_' + 'visualizaciones' + '.csv'
+        df_visualizaciones.to_csv(
+            index=False, date_format='%Y-%M-%d', path_or_buf=f'data/{filename}')
+        print(f'df_visualizaciones de {db} creado exitosamente')
+        df_usuarios = pd.read_sql('''SELECT * FROM usuarios;''', conn)
+        # filename = db+'_' + str(datetime.now().date()) + ' _usuarios' + '.csv'
+        filename = db+'_' + 'usuarios' + '.csv'
+        df_usuarios.to_csv(index=False, date_format='%Y-%M-%d',
+                           path_or_buf=f'data/{filename}')
+        print(f'df_usuarios de {db} creado exitosamente')
+        df_peliculas = pd.read_sql('''SELECT * FROM peliculas;''', conn)
+        # filename = db+'_' + str(datetime.now().date()) + '_peliculas' + '.csv'
+        filename = db+'_' + 'peliculas' + '.csv'
+        df_peliculas.to_csv(index=False, date_format='%Y-%M-%d',
+                            path_or_buf=f'data/{filename}')
     print(f'df_peliculas de {db} creado exitosamente')
